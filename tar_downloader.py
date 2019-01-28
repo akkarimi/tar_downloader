fc1394e78d5b7bac4be4350b2b9bd7547fcd47cb
Akbar Karimi <45362891+akkarimi@users.noreply.github.com>
2019-01-28 18:05:22 +0100

    Add files via upload

---
+++ tar_downloader.py
@@ -0,0 +1,20 @@
+
+from tqdm import tqdm
+import requests
+import urllib.request as ur
+from bs4 import BeautifulSoup
+
+page_link = 'https://archive.org/download/archiveteam-twitter-stream-2018-06'
+page = ur.urlopen(page_link)
+soup = BeautifulSoup(page, 'html.parser')
+
+for link in soup.find_all('a'):
+    tar_link = link.get('href')
+    if tar_link.endswith('.tar'):
+        url = 'https://archive.org/download/archiveteam-twitter-stream-2018-06/' + tar_link
+        don = requests.get(url, stream=True)
+        with open(tar_link, 'wb') as file_name:
+            for part in tqdm(don.iter_content()):
+                if part:
+                    file_name.write(part)
+
