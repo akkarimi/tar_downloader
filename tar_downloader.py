5b27d872e8ee55987f15af9f775e0ffbed2430ac
Akbar Karimi <45362891+akkarimi@users.noreply.github.com>
2019-01-28 18:33:01 +0100

    Update tar_downloader.py
    Use this python code to download tar files from a webpage.

--- tar_downloader.py
+++ tar_downloader.py
@@ -9,13 +9,19 @@ page_link = input()
 page = ur.urlopen(page_link)
 soup = BeautifulSoup(page, 'html.parser')
 
+print('These files will be downloaded:')
+for link in soup.find_all('a'):
+    tarlink = link.get('href')
+    if tarlink.endswith('.tar'):
+        print(tarlink)
+
 for link in soup.find_all('a'):
     tar_link = link.get('href')
     if tar_link.endswith('.tar'):
-        url = 'https://archive.org/download/archiveteam-twitter-stream-2018-06/' + tar_link
+        url = page_link + '/' + tar_link
+        print('\n' + tar_link + ' is being downloaded...')
         don = requests.get(url, stream=True)
         with open(tar_link, 'wb') as file_name:
             for part in tqdm(don.iter_content()):
                 if part:
                     file_name.write(part)
-
